9e6414c66cc74a9a90a431136cd82b7360422369
AmineLS <contact.aminels@gmail.com>
2023-06-18 16:05:49 +0100

    Added multi-criteria filtering to the table of submission on the Home page. Add (and changed) CSS after adding the filtering input to the Home page. Fixed a bug where the Resume preview stays the same for all submissions.

--- WeRecruit/wwwroot/js/home.js
+++ WeRecruit/wwwroot/js/home.js
@@ -2,22 +2,39 @@
 let modalTitle = $('.modal-title');
 let showButton = $(".show-button")
 let closeButton = $(".close-button");
-let resumePreviewObject = $("#resume-preview")
-let resumeDownloadButton = $("#resume-download")
+let modalBody = $(".modal-body")
+let filterInput = $("#filter-input")
+let tableRows = $("#table tbody tr");
 
-$(document).ready(function(){
-    showButton.on('click', function() {
-        resumePreviewObject.attr("data", $(this).data("resume-url"))
-        resumeDownloadButton.attr("href", $(this).data("resume-url"))
+$(document).ready(function () {
+    showButton.on('click', function () {
+        modalBody.empty()
+        modalBody.append(
+            `<object id="resume-preview" data="${$(this).data("resume-url")}">
+                <p>Unable to display file. 
+                    <a href="${$(this).data("resume-url")}" target="_blank">
+                        Download
+                    </a> 
+                instead.</p>
+            </object>`
+        )
         modalTitle.text($(this).data("resume-owner"))
         modal.fadeIn();
     });
 
-    closeButton.on('click', function() {
+    closeButton.on('click', function () {
         modal.fadeOut();
+    });
+
+    filterInput.on("keyup", function () {
+        let value = $(this).val().toLowerCase();
+        console.log(value)
+        tableRows.filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
 });
 
-$('body').bind('click', function(e){
-    if($(e.target).hasClass("modal")) modal.fadeOut();
+$('body').bind('click', function (e) {
+    if ($(e.target).hasClass("modal")) modal.fadeOut();
 });
