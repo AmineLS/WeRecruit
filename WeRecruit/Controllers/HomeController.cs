ffd4ef8083522a13b28f997cd412061731934e32
AmineLS <contact.aminels@gmail.com>
2023-06-18 14:08:01 +0100

    Added the View and the Controller for the Home page of the portal's Admin, where he/she can manage the submissions.

---
+++ WeRecruit/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WeRecruit.Services;
+
+namespace WeRecruit.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ISubmissionsService _submissionsService;
+
+    public HomeController(ISubmissionsService submissionsService)
+    {
+        _submissionsService = submissionsService;
+    }
+
+    [Route("/home")]
+    [Authorize]
+    public async Task<IActionResult> Read()
+    {
+        ViewData["UserName"] = User.Identity?.Name;
+        var submissions = await _submissionsService.ReadAll();
+        return View("Index", submissions);
+    }
+}
---
+++ WeRecruit/wwwroot/js/home.js
@@ -0,0 +1,23 @@
+﻿let modal = $('#modal-dialog');
+let modalTitle = $('.modal-title');
+let showButton = $(".show-button")
+let closeButton = $(".close-button");
+let resumePreviewObject = $("#resume-preview")
+let resumeDownloadButton = $("#resume-download")
+
+$(document).ready(function(){
+    showButton.on('click', function() {
+        resumePreviewObject.attr("data", $(this).data("resume-url"))
+        resumeDownloadButton.attr("href", $(this).data("resume-url"))
+        modalTitle.text($(this).data("resume-owner"))
+        modal.fadeIn();
+    });
+
+    closeButton.on('click', function() {
+        modal.fadeOut();
+    });
+});
+
+$('body').bind('click', function(e){
+    if($(e.target).hasClass("modal")) modal.fadeOut();
+});
